d35ed0d9c89b2bf094de049bccac49825d84c8a1
chenyihan <348800349@qq.com>
2015-11-29 21:07:13 +0800

    Add getRange,getSet,mSet,mGet,mSetNX,setNX,setEX,pSetEX,setRange,strLen command

--- test/StringCmdTest.js
+++ test/StringCmdTest.js
@@ -244,6 +244,210 @@ StringCmdTest.prototype = {
 				assert.equal(resp, 41);
 			});
 		});
+	},
+	test_getrange : function() {
+		this.client.flushAll();
+
+		this.client.set('key1', 'hello, my friend');
+		this.client.getRange('key1', 0, 4, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'hello');
+			});
+		});
+
+		this.client.getRange('key1', -1, -5, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, '');
+			});
+		});
+
+		this.client.getRange('key1', -3, -1, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'end');
+			});
+		});
+
+		this.client.getRange('key1', 0, -1, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'hello, my friend');
+			});
+		});
+
+		this.client.getRange('key1', 0, 1008611, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'hello, my friend');
+			});
+		});
+	},
+	test_getset : function() {
+		this.client.flushAll();
+
+		this.client.getSet('key1', 'value1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'null');
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value1');
+			});
+		});
+
+		this.client.getSet('key1', 'value2', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value1');
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value2');
+			});
+		});
+	},
+	test_mgetAndMset : function() {
+		this.client.flushAll();
+
+		this.client.mSet([ {
+			key : "key1",
+			value : "value1"
+		}, {
+			key : "key2",
+			value : "value2"
+		}, {
+			key : "key3",
+			value : "value3"
+		} ], function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'OK');
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value1');
+			});
+		});
+		this.client.get('key2', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value2');
+			});
+		});
+		this.client.get('key3', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value3');
+			});
+		});
+
+		this.client.mGet([ 'key1', 'key2', 'key3', 'key4' ],
+				function(resp, err) {
+					console.log(resp);
+				});
+
+		this.client.set('key1', 'value1')
+		this.client.mGet('key1', function(resp, err) {
+			console.log(resp);
+		});
+
+		this.client.mSetNX([ {
+			key : "key1",
+			value : "value1"
+		}, {
+			key : "key4",
+			value : "value4"
+		} ], function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 0);
+			});
+		});
+		this.client.mSetNX([ {
+			key : "key5",
+			value : "value5"
+		}, {
+			key : "key4",
+			value : "value4"
+		} ], function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 1);
+			});
+		});
+		this.client.setNX("key5", "value5", function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 0);
+			});
+		});
+		this.client.setNX("key6", "value6", function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 1);
+			});
+		});
+	},
+	test_setEX : function() {
+		this.client.flushAll();
+
+		this.client.setEX('key1', 100, 'value1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'OK');
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value1');
+			});
+		});
+
+		// this.client.ttl('key1', function(resp, err) {
+		// console.log(resp);
+		// });
+	},
+	test_psetEX : function() {
+		this.client.flushAll();
+
+		this.client.pSetEX('key1', 100000, 'value1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'OK');
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'value1');
+			});
+		});
+
+		// this.client.pTTL('key1', function(resp, err) {
+		// console.log(resp);
+		// });
+	},
+	test_setrange : function() {
+		this.client.flushAll()
+
+		this.client.set('key1', 'hello world')
+
+		this.client.setRange('key1', 6, 'Redis', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 11);
+			});
+		});
+
+		this.client.get('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, "hello Redis");
+			});
+		});
+	},
+	test_strlen : function() {
+		this.client.flushAll()
+
+		this.client.set('key1', 'Hello world')
+
+		this.client.strLen('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 11);
+			});
+		});
 	}
 };
 
@@ -316,6 +520,41 @@ if (typeof describe === "function") {
 				tester.test_decrby();
 			})
 		});
+		describe("#getrange", function() {
+			it("redis getrange command", function() {
+				tester.test_getrange();
+			})
+		});
+		describe("#getset", function() {
+			it("redis getset command", function() {
+				tester.test_getset();
+			})
+		});
+		describe("#mset,#mget,#msetNX,#setNX", function() {
+			it("redis mset,mget command", function() {
+				tester.test_mgetAndMset();
+			})
+		});
+		describe("#setEX", function() {
+			it("redis setEX command", function() {
+				tester.test_setEX();
+			})
+		});
+		describe("#psetEX", function() {
+			it("redis psetEX command", function() {
+				tester.test_psetEX();
+			})
+		});
+		describe("#setrange", function() {
+			it("redis setrange command", function() {
+				tester.test_setrange();
+			})
+		});
+		describe("#strlen", function() {
+			it("redis strlen command", function() {
+				tester.test_strlen();
+			})
+		});
 	});
 } else {
 	tester.test_set();
@@ -331,4 +570,11 @@ if (typeof describe === "function") {
 	tester.test_incrbyfloat();
 	tester.test_decr();
 	tester.test_decrby();
+	tester.test_getrange();
+	tester.test_getset();
+	tester.test_mgetAndMset();
+	tester.test_setEX();
+	tester.test_psetEX();
+	tester.test_setrange();
+	tester.test_strlen();
 }
