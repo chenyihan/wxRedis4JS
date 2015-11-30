bab0d6d7461a363187c8916dbae000c9b741c943
chenyihan <348800349@qq.com>
2015-11-30 23:18:25 +0800

    Add objectRefcount,objectIdletime,objectEncoding,pExpire,pExpireAt,randomKey command

--- test/KeyCmdTest.js
+++ test/KeyCmdTest.js
@@ -126,6 +126,132 @@ KeyCmdTest.prototype = {
 			});
 		});
 
+	},
+	test_object : function() {
+		this.client.flushAll();
+
+		this.client.set('key1', 'value1');
+		this.client.objectRefcount('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 1);
+			});
+		});
+
+		this.client.objectIdletime('key1', function(resp, err) {
+			console.log("idle time:" + resp);
+		});
+
+		this.client.get('key1');
+		this.client.objectIdletime('key1', function(resp, err) {
+			console.log("idle time:" + resp);
+		});
+
+		this.client.objectEncoding('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'raw');
+			});
+		});
+
+		this.client.set('key1', 158201332432423);
+		this.client.objectEncoding('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'raw');
+			});
+		});
+
+		this.client.set('key1', 20);
+		this.client.objectEncoding('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'int');
+			});
+		});
+	},
+	test_persist : function() {
+		this.client.flushAll();
+
+		this.client.set('key1', 'value1');
+		this.client.expire('key1', 10);
+
+		this.client.ttl('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp > 0 && resp <= 10, true);
+			});
+		});
+
+		this.client.persist('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, true);
+			});
+		});
+
+		this.client.ttl('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, -1);
+			});
+		});
+	},
+	test_pexpire : function() {
+		this.client.flushAll();
+		var millSeconds = 10000;
+		this.client.pExpire('key1', millSeconds, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, false);
+			});
+		});
+
+		this.client.set('key1', 'value1');
+		this.client.pExpire('key1', millSeconds, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, true);
+			});
+		});
+
+		this.client.pTTL('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp <= millSeconds, true);
+			});
+		});
+	},
+	test_PExpireAt : function() {
+		this.client.flushAll();
+		var ts = 2355292000000;
+		this.client.pExpireAt('key1', ts, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, false);
+			});
+		});
+
+		this.client.set('key1', 'value1');
+		this.client.pExpireAt('key1', ts, function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, true);
+			});
+		});
+
+		this.client.pTTL('key1', function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp <= ts, true);
+			});
+		});
+	},
+	test_randomKey : function() {
+		this.client.flushAll();
+
+		this.client.randomKey(function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.equal(resp, 'null');
+			});
+		});
+
+		this.client.set('key1', 'value1');
+		this.client.set('key2', 'value2');
+
+		this.client.randomKey(function(resp, err) {
+			baseProto.dealCmdResult(resp, err, function() {
+				assert.notEqual(resp, 'null');
+				assert.equal(resp == 'key1' || resp == 'key2', true);
+			});
+		});
 	}
 };
 
@@ -157,11 +283,41 @@ if (typeof describe === "function") {
 				tester.test_move();
 			});
 		});
+		describe("#object", function() {
+			it("Redis object command", function() {
+				tester.test_object();
+			});
+		});
+		describe("#persist", function() {
+			it("Redis persist command", function() {
+				tester.test_persist();
+			});
+		});
+		describe("#pexpire", function() {
+			it("Redis pexpire command", function() {
+				tester.test_pexpire();
+			});
+		});
+		describe("#PExpireAt", function() {
+			it("Redis PExpireAt command", function() {
+				tester.test_PExpireAt();
+			});
+		});
+		describe("#randomKey", function() {
+			it("Redis randomKey command", function() {
+				tester.test_randomKey();
+			});
+		});
 	});
 } else {
-	 tester.test_dump();
+	tester.test_dump();
 	tester.test_expire();
 	tester.test_expireAt();
 	tester.test_migrate();
 	tester.test_move();
+	tester.test_object();
+	tester.test_persist();
+	tester.test_pexpire();
+	tester.test_PExpireAt();
+	tester.test_randomKey();
 }
